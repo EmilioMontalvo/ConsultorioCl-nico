91d693b4c5b0e0c82f5bd4562a86fda0e189bafa
Carolina Pantoja <78102915+Carolina-20@users.noreply.github.com>
2023-03-14 23:43:46 -0500

    Update ArbolPacientes.java
    JavaDocs en la clase ArbolPaciente

--- src/Logica/ArbolPacientes.java
+++ src/Logica/ArbolPacientes.java
@@ -6,7 +6,10 @@ public class ArbolPacientes {
     public ArbolPacientes() {
         this.raiz = null;
     }
-
+    /** 
+     * Metodo que sirve para insertar un nodo al arbol
+     * @param paciente obejto Paciente que se agregara al arbol
+     */
     public void insertarNodo(Paciente paciente) {
         if (arbolVacio()) {
             raiz = paciente;
@@ -14,7 +17,12 @@ public class ArbolPacientes {
             raiz.enlazarNodo(paciente);
         }
     }
-
+    
+   /** 
+    * Metodo que busca un nodo en el árbol por su número de cédula
+    * @param busqueda el número de cédula a buscar en el árbol
+    * @return el nodo del árbol que contiene el número de cédula buscado, o null si no se encontró
+    */
     public Paciente buscarNodo(long busqueda) {
         if (arbolVacio()) return null;
 
@@ -37,7 +45,10 @@ public class ArbolPacientes {
         // entonces padre es el nodo previo a actualizarse
         return nodoActual;
     }
-
+   /** 
+    * Metodo que verifica si el árbol está vacío.
+    * @return true si el árbol está vacío, false en caso contrario.
+    */
     public boolean arbolVacio() {
         // Si la raiz es nulo entonces arbol vacío
         return this.raiz == null;

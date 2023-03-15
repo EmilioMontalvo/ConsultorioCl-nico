d669305846c4b90a1b168912db3df8a550255ae4
Carolina Pantoja <78102915+Carolina-20@users.noreply.github.com>
2023-03-14 23:46:10 -0500

    Update Cita.java
    JavaDocs en clase Cita

--- src/Logica/Cita.java
+++ src/Logica/Cita.java
@@ -10,6 +10,14 @@ public class Cita {
     String sintomas;
     String diagnostico;
     String prescripcion;
+    
+    /**
+    *Constructor de la clase Cita.
+    *@param paciente el objeto Paciente asociado a la cita.
+    *@param sintomas una cadena de texto que describe los síntomas del paciente.
+    *@param diagnostico una cadena de texto que describe el diagnóstico del paciente.
+    *@param prescripcion una cadena de texto que describe la prescripción médica para el paciente.
+    */
 
     public Cita(Paciente paciente, String sintomas, String diagnostico, String prescripcion) {
         this.siguiente = null;
